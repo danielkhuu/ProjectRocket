5206d736ef35ddafe1af04e2242128247023028f
Daniel Khuu <danieldkhuu@gmail.com>
2020-05-22 18:45:24 -0500

    11_PB_CU2 Adding Audio

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -10,10 +10,12 @@ public class Rocket : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();  //ref rigidbody component from Rocketship on Unity Editor
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,6 +28,14 @@ public class Rocket : MonoBehaviour
         if(Input.GetKey(KeyCode.Space))   //can thrust while rotating
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))  //cannot rotate both ways at same time
